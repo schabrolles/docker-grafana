332ad53b525263321f120c6a0daa3a94d3fe50a0
schabrolles <s.chabrolles@fr.ibm.com>
2017-01-17 14:01:22 +0100

    Update build_grafana

--- build_script/build_grafana4.sh
+++ build_script/build_grafana4.sh
@@ -21,17 +21,24 @@ cd $GOPATH/src/github.com/grafana/grafana
 if [ ! -z $1 ]; then
     git checkout $1
 fi
+
+##Building Backend
 go run build.go setup
-godep restore
+#godep restore
 #go build .
 go run build.go build
 #go run build.go build package --force
 
 ##Building frontend assets
-npm install
-npm install -g grunt-cli
-npm rebuild node-sass
-grunt --force
+npm install -g yarn
+yarn install --pure-lockfile
+npm run build
+
+#npm install
+#npm install -g grunt-cli
+#npm rebuild node-sass
+#grunt watch
+#grunt --force
 
 ##Creating optimized release packages
 #go run build.go build package
