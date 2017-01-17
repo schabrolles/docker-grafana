da5f32a790edc7e635242797e146e181e11ec460
schabrolles <s.chabrolles@fr.ibm.com>
2017-01-17 16:44:59 +0100

    New grafana build using make

--- build_script/build_grafana4.sh
+++ build_script/build_grafana4.sh
@@ -5,14 +5,9 @@
 
 apt-get update && apt-get -y upgrade
 
-apt-get install -y git golang curl phantomjs xvfb
+apt-get install -y git golang curl python phantomjs xvfb
 
 curl -sL https://nodejs.org/dist/v6.9.4/node-v6.9.4-linux-ppc64le.tar.xz | tar Jxvf - -C /usr --strip 1
-
-npm install -g grunt grunt-cli yarn
-
-Xvfb :0 -screen 0 1024x768x24 &
-export DISPLAY=:0
 
 mkdir /go
 export GOPATH=/go
@@ -23,31 +18,10 @@ cd $GOPATH/src/github.com/grafana/grafana
 if [ ! -z $1 ]; then
     git checkout $1
 fi
+Xvfb :0 -screen 0 1024x768x24 &
+export DISPLAY=:0
 
-echo
-echo "Building Grafana Backend"
-##Building Backend
-go run build.go setup
-#godep restore
-#go build .
-go run build.go build
-#go run build.go build package --force
+make all
+make test
 
-echo
-echo "Building Grafana Frontend"
-##Building frontend assets
-yarn install --pure-lockfile
-npm rebuild node-sass
-npm run build
-
-#npm install
-#npm install -g grunt-cli
-#npm rebuild node-sass
-#grunt watch
-#grunt --force
-
-##Creating optimized release packages
-#go run build.go build package
-
-## Install
 cp -rp bin conf vendor public* /opt/grafana
